d7f16bbd6727f2e31376f0f5fba7c7db1906c2d7
yalove <excia000@gmail.com>
2015-05-25 16:19:05 +0800

    仍然有错误
    warning: assignment from incompatible pointer type [enabled by default]|

---
+++ 队列 - 链式队列.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR -2
+#define OVERFLOW 2
+#define true 1
+#define false 0
+
+typedef int ElemType;
+
+
+typedef struct              //链式队列结点
+{
+    ElemType data;
+    struct LinkNode *next;
+
+}LinkNode;
+
+typedef struct              //链式队列
+{
+    LinkNode *front ,*rear; //队列头尾指针
+}LinkQueue;
+
+//初始化带头结点，可以统一操作，简洁方便
+int InitQueue(LinkQueue *Q)
+{
+    Q->front=Q->rear=malloc(sizeof(LinkNode));
+    Q->front->next=NULL;
+}
+
+//判读队空
+int isEmpty(LinkQueue Q)
+{
+    if(Q.front=Q.rear) return true;
+    else return false;
+}
+
+//入队
+int EnQueue(LinkQueue *Q,ElemType e)
+{
+    LinkNode *p =malloc(sizeof(LinkNode));
+    p->data =e;
+    p->next =NULL;
+    //warning: assignment from incompatible pointer type [enabled by default]|
+    (Q->rear)->next=p;
+    Q->rear=p;
+
+}
+
+//出队
+int DeQueue(LinkQueue *Q,ElemType *e)
+{
+    if(Q->front==Q->rear) return false;
+    LinkNode *p=Q->front->next;
+    *e=p->data;
+    Q->front->next=p->next;
+    if(Q->rear==p)
+        Q->rear=Q->front;
+    free(p);
+    return true;
+}
+
+//获取队头元素
+int GetQT(LinkQueue Q,ElemType *e)
+{
+    if(Q.front==Q.rear) return false;
+    LinkNode *p =Q.front->next;
+    *e=p->data;
+    printf("QT=%d\n",*e);
+    return true;
+}
+
+//获取队列所有元素
+int GetALL(LinkQueue Q)
+{
+    int i=0;
+    LinkNode *p =Q.front->next;
+    while(p!=Q.rear->next){
+        printf("Q[%d]=%d\n",i,p->data);
+        i++;
+        LinkNode *s=p;
+        p=s->next;
+    }
+}
+int main()
+{
+    int i=0;
+    ElemType e,x;
+    LinkQueue Q;
+    InitQueue(&Q);
+    isEmpty(Q);
+    for(i;i<5;i++){
+        scanf("%d",&x);
+        EnQueue(&Q,x);
+    };
+    GetQT(Q,&e);
+    GetALL(Q);
+    printf("出队两个元素-------\n");
+    DeQueue(&Q,&e);
+    DeQueue(&Q,&e);
+    GetALL(Q);
+
+}
